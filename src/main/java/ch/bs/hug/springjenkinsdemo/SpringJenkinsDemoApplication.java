745a84d5421dc555e5511127f8aafa4ba24a9bc3
Florian Hug <florian.hug@bs.ch>
2022-03-16 12:17:31 +0100

    init

---
+++ src/main/java/ch/bs/hug/springjenkinsdemo/SpringJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package ch.bs.hug.springjenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsDemoApplication {
+
+  public static void main (String[] args) {
+    SpringApplication.run(SpringJenkinsDemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ch/bs/hug/springjenkinsdemo/SpringJenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bs.hug.springjenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsDemoApplicationTests {
+
+  @Test
+  void contextLoads () {
+  }
+
+}
