917a0bea49fa24d6c5552f6657033527d1832f2b
Florian Hug <florian.hug@bs.ch>
2022-03-17 14:33:42 +0100

    docker fix with annotations

--- src/main/java/ch/bs/hug/springjenkinsdemo/SpringJenkinsDemoApplication.java
+++ src/main/java/ch/bs/hug/springjenkinsdemo/SpringJenkinsDemoApplication.java
@@ -1,9 +1,16 @@
 package ch.bs.hug.springjenkinsdemo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@EnableWebMvc
+@Configuration
+@ComponentScan
 public class SpringJenkinsDemoApplication {
 
   public static void main (String[] args) {
