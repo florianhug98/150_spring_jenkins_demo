cf3502f21d4e91c6d0c6a52533930e245c9aefc0
Florian Hug <florian.hug@bs.ch>
2022-03-17 14:36:26 +0100

    revert

--- src/main/java/ch/bs/hug/springjenkinsdemo/SpringJenkinsDemoApplication.java
+++ src/main/java/ch/bs/hug/springjenkinsdemo/SpringJenkinsDemoApplication.java
@@ -1,16 +1,9 @@
 package ch.bs.hug.springjenkinsdemo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableAutoConfiguration
-@EnableWebMvc
-@Configuration
-@ComponentScan
+@SpringBootApplication
 public class SpringJenkinsDemoApplication {
 
   public static void main (String[] args) {
